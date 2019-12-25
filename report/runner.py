ffa20bfca16432d9c85985198614935eb2ad5337
wuji152404 <656061175@qq.com>
2019-12-25 14:33:58 +0800

    run runner 1

--- report/runner.py
+++ report/runner.py
@@ -7,16 +7,12 @@ from BeautifulReport import BeautifulReport
 import unittest , os
 import time
 
-print(os.path.dirname(__file__))
-# case_path=os.path.dirname(os.path.dirname(__file__)) + '/case'
-# # 持续集成时不能用相对路径
-# report_path=os.path.dirname(os.path.dirname(__file__)) + '/report'
-# print(case_path)
-# print(report_path)
-# discover=unittest.defaultTestLoader.discover(case_path, pattern='*_case.py')
-# times=time.strftime('%Y%m%d%H%M%S')
-# filename=times + 'result.html'
-# BeautifulReport(discover).report('测试报告', filename=filename, report_dir=report_path)
+case_path='..'+ '/case'
+report_path='..' + '/report'
+discover=unittest.defaultTestLoader.discover(case_path, pattern='*_case.py')
+times=time.strftime('%Y%m%d%H%M%S')
+filename=times + 'result.html'
+BeautifulReport(discover).report('测试报告', filename=filename, report_dir=report_path)
 
 
 
